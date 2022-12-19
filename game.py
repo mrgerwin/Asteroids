c3f262cfe44b99c678ddf059a9fe8b1b5962d0b8
mrgerwin <jgerwin@otsegoknights.org>
2022-12-19 10:23:51 -0500

    Initial game file

--- game.py
+++ game.py
@@ -1,1 +1,29 @@
+from pygame import *
 
+class Ship:
+    def __init__(self, position):
+        self.shipImage = image.load("Frigate3.png")
+        self.rect = window.blit(self.shipImage, [400,400])
+    
+    def drawShip(self):
+        self.rect = window.blit(self.shipImage, [400,400])
+
+screen_size = [800,800]
+window = display.set_mode(screen_size)
+
+timer = time.Clock()
+black = [0,0,0]
+
+player = Ship([400,400])
+
+while True:
+    for event in event.get():
+        if event.type == QUIT:
+            quit = True
+            quit()
+            sys.exit(0)
+    
+    window.fill(black)
+    player.drawShip()
+    display.flip()
+    timer.tick(60)
