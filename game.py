69b62ccc1227d74b3f88abdc7aab2e63f46cf1c2
25kjones <94398810+25kjones@users.noreply.github.com>
2022-12-20 10:32:27 -0500

    laserupdate

--- game.py
+++ game.py
@@ -7,6 +7,16 @@ class Ship:
     
     def drawShip(self):
         self.rect = window.blit(self.shipImage, [400,400])
+        
+        
+        
+class Lasers:
+    def __init__(self, position):
+        self.Laserimage = image.load("LaserBeam.png")
+        self.rect = window.blit(self.Laserimage,[100,100])
+        
+    def drawLaser(self):
+        self.rect = window.blit(self.Laserimage, [100,100])
 
 screen_size = [800,800]
 window = display.set_mode(screen_size)
@@ -15,6 +25,7 @@ timer = time.Clock()
 black = [0,0,0]
 
 player = Ship([400,400])
+Laser1 = Lasers([100,100])
 
 while True:
     for event in event.get():
@@ -25,5 +36,6 @@ while True:
     
     window.fill(black)
     player.drawShip()
+    Laser1.drawLaser()
     display.flip()
     timer.tick(60)
