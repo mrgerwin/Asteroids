495fb365fad0d5dc9e8297d0639ddb6631eb2bc3
25kjones <25ragosti@otsegoknights.org>
2022-12-20 10:26:09 -0500

    kewl stuff
    music files, a new frigate, and a Background!

--- game.py
+++ game.py
@@ -2,19 +2,34 @@ from pygame import *
 
 class Ship:
     def __init__(self, position):
-        self.shipImage = image.load("Frigate3.png")
+        self.shipImage = image.load("NewFrigate3_74.png")
         self.rect = window.blit(self.shipImage, [400,400])
     
     def drawShip(self):
         self.rect = window.blit(self.shipImage, [400,400])
-
+class Background:
+    def __init__(self,position):
+        self.BgImage = image.load("Background.jpg")
+        self.BgRect = window.blit(self.BgImage,[0,0])
+    
+    def drawBg(self):
+        self.BgRect = window.blit(self.BgImage,[0,0])
+    
 screen_size = [800,800]
 window = display.set_mode(screen_size)
+
+#sound Attributes
+mixer.init()
+theMusic = mixer.music.load("LeEpicBattleMusic.mp3")
 
 timer = time.Clock()
 black = [0,0,0]
 
 player = Ship([400,400])
+
+Background = Background([0,0])
+
+mixer.music.play(-1)
 
 while True:
     for event in event.get():
@@ -24,6 +39,7 @@ while True:
             sys.exit(0)
     
     window.fill(black)
+    Background.drawBg()
     player.drawShip()
     display.flip()
     timer.tick(60)
