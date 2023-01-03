739f14dd82a003812fcce4d353acf61f0049d0de
22nsnyder <25dstuder@otsegoknights.org>
2023-01-03 10:30:09 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,22 @@
 import pygame 
+import random
 
 class Asteroid:
     def __init__(self, position, size):
         self.position = position
         self.size = size
-        self.AsteroidImage = image.load("Asteroid.png")
+        self.AsteroidImage = pygame.image.load("Asteroid.png")
+        self.AsteroidImage = pygame.transform.rotozoom(self.AsteroidImage, 0, .175)
+
         
     def drawAsteroid(self):
         self.rect = window.blit(self.AsteroidImage, self.position)
+        
+    def AsteroidMove(self, postition):
+            self.position += 1
+        
+            
+            
         
     def Split():
         if self.rect.colliderect(Laser):
@@ -21,8 +30,14 @@ class Asteroid:
             
             
     def AsteroidTeleport(self, position):
-        if self.position == 0:
-            self.position = 800
+        if self.position[0] == 0:
+            self.position[0] = 800
+        if self.position[0] == 800:
+            self.position[0] = 0
+        if self.position[1] == 0:
+            self.position[1] = 800
+        if self.position[1] == 800:
+            self.position[1] = 0
 
 class Ship:
     def __init__(self, position):
@@ -40,7 +55,9 @@ class Ship:
 screen_size = [800,800]
 window = pygame.display.set_mode(screen_size)
 
-asteroid1 = 
+AsteroidSpeeds = [[0,1],[0,2],[0,3],[1,1], [1,2], [1,3], [-1, 1], [-1, 2], [-1, 3], [2, 1], [2, 2], [2, 3], [-2, 1], [-2, 2], [-2, 3]]
+SpeedX = 0
+SpeedY = 0
 topScreen = 0
 bottomScreen = 0
 leftScreen = 0
@@ -49,8 +66,8 @@ timer = pygame.time.Clock()
 black = [0,0,0]
 
 player = Ship([400,400])
-
-asteroids = []
+Asteroid1 = Asteroid([0,0], 10)
+asteroids = [Asteroid1]
 
 while True:
     for event in pygame.event.get():
@@ -65,6 +82,7 @@ while True:
     player.drawShip()
     player.moveShip()
     for asteroid in asteroids:
+        asteroid.AsteroidMove(asteroid.position)
         asteroid.drawAsteroid()
     pygame.display.flip()
     timer.tick(60)
