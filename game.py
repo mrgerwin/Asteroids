841d07a5605b3f697ff2910a7083fa0041be9e4f
24EStahl <94398435+Ethan-Stahl@users.noreply.github.com>
2023-01-04 13:13:02 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
 from pygame import *
 
+def drawText():
+    global Text, white
+    thewordsthatiuseText = Text.render("OBJECTIVE: DESTROY THE METEORS", True, white)
+    
+    window.blit(thewordsthatiuseText, (130, 20))
+
+def drawScore():
+    global score, white
+    controlText = Text.render("SPACE = Shoot         Up/Down Arrow to Move", True, white)
+    
+    window.blit(controlText, (35, 700))
+
 class Ship:
     def __init__(self, position):
         self.shipImage = image.load("Frigate3.png")
@@ -13,8 +25,12 @@ window = display.set_mode(screen_size)
 
 timer = time.Clock()
 black = [0,0,0]
+white = [255, 255, 255]
 
 player = Ship([400,400])
+#these spaceness things
+font.init()
+Text = font.SysFont("consolas", 30)
 
 while True:
     for event in event.get():
@@ -22,8 +38,12 @@ while True:
             quit = True
             quit()
             sys.exit(0)
+            
+
     
     window.fill(black)
     player.drawShip()
+    drawText()
+    drawScore()
     display.flip()
     timer.tick(60)
