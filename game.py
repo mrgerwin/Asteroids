d0c5ba184d5e5564b6660f240aaf29108f9e6f11
25kjones <94398810+25kjones@users.noreply.github.com>
2023-01-04 13:00:04 -0500

    add laser

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
-from pygame import *
+import pygame
+import math
 
 class Ship:
     def __init__(self, position):
-        self.shipImage = image.load("Frigate3.png")
+        self.shipImage = pygame.image.load("Frigate3.png")
         self.rect = window.blit(self.shipImage, [400,400])
     
     def drawShip(self):
@@ -11,31 +12,54 @@ class Ship:
         
         
 class Lasers:
-    def __init__(self, position):
-        self.Laserimage = image.load("LaserBeam.png")
-        self.rect = window.blit(self.Laserimage,[100,100])
+    def __init__(self, position, angle):
+        self.position = position
+        self.Laserimage = pygame.image.load("LaserBeam.png")
+        self.angle = angle
+        
+        self.Laserimage = pygame.transform.rotate(self.Laserimage, self.angle)
+        self.rect = window.blit(self.Laserimage,self.position)
         
     def drawLaser(self):
-        self.rect = window.blit(self.Laserimage, [100,100])
+        self.rect = window.blit(self.Laserimage, self.position)
+        
+    
+    
+    def rotate(self, angle):
+        self.Laserimage = pygame.transform.rotate(self.OriginalImage, self.angle)
+    
+    def Shoot(self):
+        self.position[0]+= math.cos((self.angle*math.pi)/180)
+        self.position[1]-= math.sin((self.angle*math.pi)/180)
 
 screen_size = [800,800]
-window = display.set_mode(screen_size)
+window = pygame.display.set_mode(screen_size)
 
-timer = time.Clock()
-black = [0,0,0]
+timer = pygame.time.Clock()
+black = [0 ,0,0]
 
 player = Ship([400,400])
-Laser1 = Lasers([100,100])
+lasers = []
 
 while True:
-    for event in event.get():
-        if event.type == QUIT:
-            quit = True
-            quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit = True
+            pygame.quit()
             sys.exit(0)
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("Pressed space")
+                Laser1 = Lasers([100,100], 45)
+                lasers.append(Laser1) 
+                
+            
     
     window.fill(black)
     player.drawShip()
-    Laser1.drawLaser()
-    display.flip()
+    for laser in lasers:
+        laser.Shoot()
+        laser.drawLaser()
+    pygame.display.flip()
     timer.tick(60)
